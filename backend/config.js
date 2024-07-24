3ba4f2bf68734c00d233ed241106e2d2fb8a983d
Diviya Pandiyan <kdmail9@gmail.com.com>
2024-07-24 22:45:49 +0530

    ready to deplaoy

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@bookstore-mern.jrcsuqt.mongodb.net/book-collection?retryWrites=true&w=majority';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
